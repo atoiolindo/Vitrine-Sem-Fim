47161e151a9a1004c205c11e6419e75cf706e0e4
gab99999 <163896436+gab99999@users.noreply.github.com>
2024-11-04 09:57:06 -0300

    gab home 2
    mudei de home.html pra home.php

--- vitrinesemfim/public/home.php
+++ vitrinesemfim/public/home.php
@@ -19,9 +19,9 @@ require_once "../controle/verificarlogado.php";
     <div class="corpo">
         <fieldset>
             <legend>Cadastrar</legend>
-            <a href="cliente.html" class="btn btn-secondary">Cadastrar cliente</a>
+            <a href="cliente.php" class="btn btn-secondary">Cadastrar cliente</a>
             <a href="autorform.php" class="btn btn-secondary">Cadastrar autor</a>
-            <a href="tabela_vendedor.html" class="btn btn-secondary">Cadastrar vendedor</a>
+            <a href="form_vendedor.php" class="btn btn-secondary">Cadastrar vendedor</a>
             <a href="formpagamento.php" class="btn btn-secondary">Cadastrar pagamento</a>
             <a href="formemprestimo.php" class="btn btn-secondary">Cadastrar empréstimo</a>
             <a href="formlivro.php" class="btn btn-secondary">Cadastrar livro</a>
