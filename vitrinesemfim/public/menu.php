118891cbb48c07f1a6c9bafb26b4cd73042fcc92
yasmin <liroyasmin@gmail.com>
2024-11-08 10:14:05 -0300

    criando menu lateral

---
+++ vitrinesemfim/public/menu.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+</head>
+<body>
+
+    <nav class="menulateral">
+
+        <div class="btnexpandir">
+            <i class="bi bi-list"></i>
+        </div>
+
+        <ul>
+            <li class="itemmenu">
+                <a href="#">
+                    <span class= "icon"></span>
+                    <span class= "txtlink"></span>
+
+                </a>
+            </li>
+        
+            <li class="itemmenu">
+                <a href="#">
+                    <span class= "icon"> <i class="bi bi-house"></i> </span>
+                    <span class= "txtlink"> Página Inicial </span>
+                </a>
+            </li>  
+            <li class="itemmenu">
+                <a href="#">
+                    <span class= "icon"></span>
+                    <span class= "txtlink"></span>
+                </a>
+             </li>  
+             <li class="itemmenu">
+                <a href="#">
+                    <span class= "icon"></span>
+                    <span class= "txtlink"></span>
+
+                </a>
+            </li>  
+            <li class="itemmenu">
+                <a href="#">
+                    <span class= "icon"></span>
+                    <span class= "txtlink"></span>
+
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+
+
+</body>
+</html>
