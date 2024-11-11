1970f16aadb7cff1f08373e3be6d7a0359a96591
yasmin <liroyasmin@gmail.com>
2024-11-11 10:03:09 -0300

    arrumando o editar no livro

--- vitrinesemfim/public/formlivro.php
+++ vitrinesemfim/public/formlivro.php
@@ -5,14 +5,16 @@ if (isset($GET['id'])) {
     require_once "../controle/conexao.php";
 
     $id = $GET['id'];
-    $sql = "SELECT * FROM paciente WHERE idpaciente = $id";
+    $sql = "SELECT * FROM livro WHERE idlivro = $id";
     $resultado = mysqli_query( $conexao, $sql);
 
     $linha = mysqli_fetch_array($resultado);
 
     $nome = $linha['nome'];
-    $cpf = $linha['cpf'];
-    $telefone = $linha['telefone'];
+    $genero = $linha['genero'];
+    $idautor = $linha['autor_idautor'];
+    $isbn = $linha ['isbn'];
+    $estado = $linha ['estado'];
 
     $botao = "Salvar";
 } else {
@@ -75,21 +77,29 @@ if (isset($GET['id'])) {
                         $sql = "SELECT idautor, nome FROM autor";
 
                         $resultados = mysqli_query($conexao, $sql);
-                        $i = 0;
 
                         while ($linha = mysqli_fetch_array($resultados)) {
-                            $i++;
-                            $id = $linha['idautor'];
+
+                            $id2 = $linha['idautor'];
                             $nome = $linha['nome'];
+                            
+                            if ($id2 == $idautor) {
+                                $selecionado = 'selected';
+                            } else {
+                                $selecionado = '';
+                            }
 
 
                             echo "<tbody>";
                             echo "<tr>";
                             echo "<th scope='row'>$i</th>";
                             echo "<td>$nome</td>";
-                            echo "<td>$id</td>";
+                            echo "<td>$id2</td>";
                             echo "</tr>";
                             echo "</tbody>";
+
+                            echo "<option value='$id2' $selecionado>$nome</option>";
+
                         }
                         ?>
                     </select>
