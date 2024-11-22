9dc23f7e57b674e2c6e28c8facfaf72e855fc4b5
yasmin <liroyasmin@gmail.com>
2024-11-22 10:59:57 -0300

    arrumando os pereco

--- vitrinesemfim/controle/deslogar.php
+++ vitrinesemfim/controle/deslogar.php
@@ -2,5 +2,4 @@
 session_start(); // Inicia a sessão
 session_destroy(); // Destrói todas as variáveis de sessão
 header("Location: ../public/index.php"); // Redireciona o usuário para a página inicial
-exit(); // Encerra a execução do script PHP
 ?>
