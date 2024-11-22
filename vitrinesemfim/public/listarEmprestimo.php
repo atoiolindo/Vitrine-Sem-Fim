2527d0962d875ced64db177ce9cf5f717bdf00f4
yomirkz <izaxxxsouza7777@gmail.com>
2024-11-22 10:37:04 -0300

    id pelo nome no listar emprestimo

--- vitrinesemfim/public/listarEmprestimo.php
+++ vitrinesemfim/public/listarEmprestimo.php
@@ -38,14 +38,37 @@
             $livro = $linha['livro_idlivro'];
             $vendedor = $linha['vendedor_idvendedor'];
 
+             // SELECT nome FROM cliente WHERE idpaciente = 1;
+             $cliente = $linha['cliente_idcliente'];
+             $sql2 = "SELECT nome FROM cliente WHERE idcliente = $cliente";
+             $resultados2 = mysqli_query($conexao, $sql2);
+             $linha2 = mysqli_fetch_array($resultados2);
+             $cliente_nome = $linha2['nome'];
+ 
+             // SELECT nome FROM livro WHERE idmedico = 1;
+             $livro = $linha['livro_idlivro'];
+             $sql3 = "SELECT nome FROM livro WHERE idlivro = $livro";
+             $resultados3 = mysqli_query($conexao, $sql3);
+             $linha3 = mysqli_fetch_array($resultados3);
+             $livro_nome = $linha3['nome'];
+
+               // SELECT nome FROM vendedor WHERE idmedico = 1;
+             $vendedor = $linha['vendedor_idvendedor'];
+             $sql4 = "SELECT nome FROM vendedor WHERE idvendedor = $vendedor";
+             $resultados4 = mysqli_query($conexao, $sql4);
+             $linha4 = mysqli_fetch_array($resultados4);
+             $vendedor_nome = $linha4['nome'];
+
+
+
             echo "<tbody>";
             echo "<tr>";
             echo "<th scope='row'>$id</th>";
             echo "<td>$data</td>";
             echo "<td>$emprestimo</td>";
-            echo "<td>$cliente</td>";
-            echo "<td>$livro</td>";
-            echo "<td>$vendedor</td>";
+            echo "<td>$cliente_nome</td>";
+            echo "<td>$livro_nome</td>";
+            echo "<td>$vendedor_nome</td>";
             echo "<td><a href='../controle/deletarEmprestimo.php?id=$id' class='btn btn-danger'>Apagar</a></td>";
             echo "<td><a href='formemprestimo.php?id=$id'>Editar</a></td>";
             echo "</tr>";
