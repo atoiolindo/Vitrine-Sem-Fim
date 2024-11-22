2e2a343cc648ac8c3eab03e6a0bba81aff7277ae
yomirkz <izaxxxsouza7777@gmail.com>
2024-11-22 10:46:42 -0300

    i

--- vitrinesemfim/public/pesquisarLivro.php
+++ vitrinesemfim/public/pesquisarLivro.php
@@ -56,6 +56,9 @@ if (isset($_GET['valor'])) {
                 $isbn = $linha['isbn'];
                 $estado = $linha['estado'];
 
+             
+        
+
 
                 echo "<tr>";
                 echo "<td>$id</td>";
